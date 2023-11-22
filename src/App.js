f55a8f863f6469bc3357fb2abe9d374b890dfb0e
devsoni-20 <devsoni4545@gmail.com>
2023-11-22 22:59:27 +0530

    video 7 content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
     <>
     <Navbar title = "Dev App's" aboutText = "About Dev App's"/>
     {/* <Navbar/> */}
+    <div className="container my-3">
+      <TextForm heading = "Enter the text to analyze below"/>
+    </div>
     </>
   );
 }
